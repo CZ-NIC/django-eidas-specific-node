babada23c43669e2537b650c8f6a34adf4dfb518
Jiří Janoušek <jiri.janousek@nic.cz>
2019-08-12 10:19:18 +0200

    fixup! Add Django app backbone

--- eidas_proxy_service/tests/__init__.py
+++ eidas_proxy_service/tests/__init__.py
@@ -1,10 +1,1 @@
 """Tests of eidas_proxy_service."""
-from unittest import TestCase
-
-
-class TestDummy(TestCase):
-    """A dummy test case to test coverage report."""
-
-    def test_dummy(self):
-        """A dummy test."""
-        self.assertTrue(True)
