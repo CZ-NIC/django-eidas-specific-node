035d3366c30a4c4f5579d01db4bfd5640a348cc7
Jiří Janoušek <jiri.janousek@nic.cz>
2019-09-05 17:48:26 +0200

    Add Demo Service Provider views
    Ref #11

--- eidas_node/tests/urls.py
+++ eidas_node/tests/urls.py
@@ -1,5 +1,5 @@
 """Django URLs for unittests."""
-from eidas_node.connector import urls as connector_urls
+from eidas_node.connector.demo import urls as connector_urls
 from eidas_node.proxy_service import urls as proxy_service_urls
 
 urlpatterns = connector_urls.urlpatterns + proxy_service_urls.urlpatterns
