4b141b2b68dcb237be1c841f1ee8498d0d04aa62
Jiří Janoušek <jiri.janousek@nic.cz>
2019-09-24 16:13:10 +0200

    Add CZ.NIC branding
    Ref #59

---
+++ eidas_node/cznic/__init__.py
@@ -0,0 +1,6 @@
+"""CZ.NIC branding."""
+from os.path import abspath, dirname, join
+
+BASE_DIR = dirname(abspath(__file__))
+TEMPLATE_DIR = join(BASE_DIR, 'templates')
+STATIC_DIR = join(BASE_DIR, 'static')
