3b3b47c245195cdba8d95a11fe4f2884613a1e23
Tomas Divis <tomas.divis@nic.cz>
2023-01-03 15:26:22 +0100

    Fix #119 - Fix tox.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='eidas_node',
       classifiers=[
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
       ],
-      python_requires='~=3.5',
+      python_requires='~=3.8',
       packages=find_packages(),
       include_package_data=True,
       install_requires=open('requirements.txt').read().splitlines(),
